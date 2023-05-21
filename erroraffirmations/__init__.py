811304b5f39c80594603c96ae533996595f69a78
Thomas Gessey-Jones <tg400@cam.ac.uk>
2023-05-21 17:54:32 +0100

    stop __init__ triggering flake8 error

--- erroraffirmations/__init__.py
+++ erroraffirmations/__init__.py
@@ -1,2 +1,2 @@
 """ ErrorAffirmations: To help you feel better about your errors """
-from erroraffirmations.affirmations import affirmations
+from erroraffirmations.affirmations import affirmations  # noqa
